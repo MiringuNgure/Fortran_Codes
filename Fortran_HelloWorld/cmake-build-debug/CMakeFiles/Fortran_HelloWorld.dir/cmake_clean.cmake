ef605793f8207dad5f49de5217f61b602bd2369d
Peter Ngure <peter.ngure@outlook.com>
2021-02-27 00:48:50 +0100

    Hello world Fortran

---
+++ Fortran_HelloWorld/cmake-build-debug/CMakeFiles/Fortran_HelloWorld.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Fortran_HelloWorld.dir/main.f90.obj"
+  "Fortran_HelloWorld.exe"
+  "Fortran_HelloWorld.exe.manifest"
+  "Fortran_HelloWorld.pdb"
+  "libFortran_HelloWorld.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang Fortran)
+  include(CMakeFiles/Fortran_HelloWorld.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Fortran_HelloWorld/main.f90
@@ -0,0 +1,7 @@
+! Hello worls in fortran 90
+! Authored by Peter Ngure
+
+program Fortran_HelloWorld
+    write(*,*) "Hello, World!" ! one option of printing to the screen
+    print*, "This is another Hello! Hello World" ! second option of printing to screen
+end program
