4fa240103e9e201c4aa76cd079b34c9de8ad8808
Peter Ngure <peter.ngure@outlook.com>
2021-02-28 16:21:44 +0100

    Exercise 3.1

---
+++ ex3_1/cmake-build-debug/CMakeFiles/ex3_1.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ex3_1.dir/main.f90.obj"
+  "ex3_1.exe"
+  "ex3_1.exe.manifest"
+  "ex3_1.pdb"
+  "libex3_1.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang Fortran)
+  include(CMakeFiles/ex3_1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
